e09d74c2eb1e886fa3dc247ce9bbb29f45804370
Marvin Vrdoljak <marvin.vrdoljak@dudagroup.com>
2025-03-25 16:12:38 +0100

    restructure layout and header components, remove unused styles, and implement new typography

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
 import Providers from './providers'
 import { routing } from '@/i18n/routing'
 import getRequestConfig from '@/i18n/request'
-import Header from '@/components/globals/Header/Header'
+import Header from '@/components/globals/Header'
 import '@/styles/globals.scss'
+import { Poppins } from 'next/font/google'
 
 export async function generateStaticParams() {
   return routing.locales.map((locale) => ({
@@ -15,11 +16,18 @@ interface RootLayoutProps {
   params: { locale: string }
 }
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-poppins',
+  display: 'swap',
+})
+
 export default async function RootLayout({ children, params }: RootLayoutProps) {
   const localeConfig = await getRequestConfig({ requestLocale: Promise.resolve(params.locale) })
 
   return (
-    <html lang={localeConfig.locale}>
+    <html lang={localeConfig.locale} className={poppins.variable}>
       <Providers messages={localeConfig?.messages as any} locale={localeConfig.locale}>
         <body>
           <Header />
--- src/components/globals/Header.tsx
+++ src/components/globals/Header.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import LangSwitch from '../LangSwitch/LangSwitch'
+import LangSwitch from './LangSwitch'
 
 export default function Header() {
   return (
