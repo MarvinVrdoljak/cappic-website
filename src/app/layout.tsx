e8294e28a7836c7466d3d902b2cd4817ea65bcda
Marvin Vrdoljak <marvin.vrdoljak@dudagroup.com>
2025-04-19 07:40:54 +0200

    404 page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
 // "app/layout.tsx"
+import '@/styles/globals.scss'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-poppins',
+  display: 'swap',
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children
+  return <html className={poppins.variable}>{children}</html>
 }
