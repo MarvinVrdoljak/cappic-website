34eb507831a1b76bea5c89286480d2fc59bb3041
Marvin Vrdoljak <marvin.vrdoljak@dudagroup.com>
2025-04-19 10:52:19 +0200

    Enhance login and root layout to support deep linking. Implement fallback for app not installed scenario and extract path after /login for better URL handling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 // "app/layout.tsx"
 import Header from '@/components/globals/Header'
 import '@/styles/globals.scss'
 import { Poppins } from 'next/font/google'
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -11,6 +15,26 @@ const poppins = Poppins({
 })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    if (pathname?.includes('/login/')) {
+      // Extrahiere den Pfad nach /login/
+      console.log(pathname)
+      const deepLink = `cappic-app://${pathname}`
+
+      // Versuche, die App über das App-Schema zu öffnen
+      window.location.href = deepLink
+
+      // Fallback nach 1.5 Sekunden
+      const timeout = setTimeout(() => {
+        window.location.href = 'https://cappic.app/en/app-not-installed'
+      }, 1500)
+
+      return () => clearTimeout(timeout)
+    }
+  }, [pathname])
+
   return (
     <html className={poppins.variable}>
       <body>
